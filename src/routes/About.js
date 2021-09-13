cd2ff777ed3a1b3de98cb2765ae1b559c0f7f5da
DConstantine9 <constantinedeniskkin@gmail.com>
2021-09-13 20:12:42 +0300

    styles and routes

---
+++ src/routes/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about__container">О проекте: я сделал этот проект, потому что люблю фильмы</div>
+  )
+}
+
+export default About;
