77ebe31ed650d08c94e2bca114f1c4098beabaf7
DConstantine9 <constantinedeniskkin@gmail.com>
2021-09-14 20:23:08 +0300

    adaptivity

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -15,7 +15,9 @@ function Detail(props) {
   if (locationState) {
       return (
         <div className="detail__block">
-          <img src={locationState.poster} alt={locationState.title} title={locationState.title} />
+          <div className="detail__poster"> 
+            <img src={locationState.poster} alt={locationState.title} title={locationState.title} />
+          </div>
           <div className="detail__info">
             <h1 className="detail__title">{locationState.title}</h1>
             <h4 className="detail__year">{locationState.year}</h4>
